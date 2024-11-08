dec4de8c0b4a8910e5c013eaafe8b7f44da94739
progr <programtiago@gmail.com>
2024-11-08 10:18:04 +0000

    First commit

---
+++ backend/src/test/java/com/netceed/management/management_app/ManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netceed.management.management_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
