910b3577162d289f9d4b9426e0e0c7deb3f17fd3
progr <programtiago@gmail.com>
2024-11-21 15:44:00 +0000

    Refactoring Material Table

--- frontend/src/app/admin/services/admin.service.ts
+++ frontend/src/app/admin/services/admin.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { User } from '../../model/user';
-import { delay, first, tap } from 'rxjs';
+import { first } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
