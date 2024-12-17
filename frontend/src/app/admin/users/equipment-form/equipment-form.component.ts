235965fee7bada7e8191abf03ba44b1c2adb3c15
progr <programtiago@gmail.com>
2024-12-17 12:56:09 +0000

    Reactive Form implemented for equipment. Form already submits equipment object on db and its assigned to a user.

--- frontend/src/app/admin/users/equipment-form/equipment-form.component.ts
+++ frontend/src/app/admin/users/equipment-form/equipment-form.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, ViewChild, viewChild } from '@angular/core';
-import { FormControl } from '@angular/forms';
-import { MatSelect, MatSelectChange } from '@angular/material/select';
+import { Component, OnInit } from '@angular/core';
+import { MatSelectChange } from '@angular/material/select';
 import { User } from '../../../model/user';
-import { Observable } from 'rxjs';
 import { AdminService } from '../../services/admin.service';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { CreateEquipmentAssignUserRequest } from '../../../model/equipment-create-assign-user';
+import { Equipment } from '../../../model/equiment';
 
 @Component({
   selector: 'app-equipment-form',
@@ -12,12 +13,32 @@ import { AdminService } from '../../services/admin.service';
 })
 export class EquipmentFormComponent implements OnInit{
 
-  selectedValueCreationWithAssignment: any;
-  selectedValueDepartment: any;
-  selectedUserId: any;
+  selectedValueCreationWithAssignment: any
+  selectedValueDepartment: any
+  selectedUserId: any
+  selectedCategory: any
+  selectedWorkstation: any
+  typeAssignmentSelected: string = '';
+
   users: User[] = [];
 
-  constructor(private adminSerice: AdminService){}
+  createEquipmentForm!: FormGroup;
+  
+
+  categorys: string[] = ['Scanner', 'Desktop', 'Printer', 'Dockstation', 'Screen', 'Laptop']
+  workstations: string[] = ['Unity A', 'Unity B', 'Unity C']
+
+  constructor(private adminSerice: AdminService, private fb: FormBuilder){
+    this.createEquipmentForm = this.fb.group({
+      description: [''],
+      serialNumber: [''],
+      brand: [''],
+      model: [''],
+      user: [''],
+      category: [''],
+      unity: [''], 
+    })
+  }
 
   ngOnInit(): void {
     this.adminSerice.listUsers().subscribe((res) => {
@@ -41,15 +62,19 @@ export class EquipmentFormComponent implements OnInit{
     this.selectedValueDepartment = this.selectedValueDepartment.text
   }
 
-  checkId(){
-    console.log(this.selectedUserId)
+  
+  createEquipmentAndAssignToUser(userId: number){
+    const newEquipment: CreateEquipmentAssignUserRequest = this.createEquipmentForm.value;
+    if (this.selectedValueCreationWithAssignment == 'User'){
+      console.log("USER SELECTED")
+      if (this.createEquipmentForm.valid){
+        console.log(this.createEquipmentForm.value)
+        this.adminSerice.createEquipmentAndAssignToUser(newEquipment, userId).subscribe((res) => {
+          console.log("Sucessfull response" + res)
+        }), (error: any) => {
+          console.log(error)
+        }
+      }
+    }
   }
-
-  create(){
-    this.adminSerice.getUserById(this.selectedUserId).subscribe((res) => {
-      console.log(res)
-    })
-  }
-
-
 }
--- frontend/src/app/model/equipment-create-assign-user.ts
+++ frontend/src/app/model/equipment-create-assign-user.ts
@@ -2,6 +2,7 @@ import { User } from "./user";
 
 export interface CreateEquipmentAssignUserRequest{
     id: number,
+    serialNumber: string,
     description: string,
     brand: string,
     model: string,
